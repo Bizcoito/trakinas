f5d7700429d20c364aed9e728809c8c7315eafb5
LeoSL <leosilvalima@gmail.com>
2017-07-09 21:51:10 -0400

    add search bar and reafactor Google API request function

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import BookList from './components/book_list';
+import BookList from './components/book-list';
+import SearchBar from './components/search-bar';
 import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
     super(props);
-
+    this.googleBooksEndpoint = 'https://www.googleapis.com/books/v1/volumes'
+    this.googleApiKey = 'AIzaSyCdJvgLdKZHXr_59YEyRv4H1z1La2uzvk0';
     this.state = {
       books: []
     };
+
+    this.bookSearch('flowers');
   }
 
-  componentDidMount() {
-    axios.get('https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyCdJvgLdKZHXr_59YEyRv4H1z1La2uzvk0')
+  bookSearch(searchTerm) {
+    axios.get(`${this.googleBooksEndpoint}?q=${searchTerm}&key=${this.googleApiKey}`)
       .then(response => {
         const books = response.data.items
         this.setState({ books });
@@ -24,8 +28,11 @@ class App extends Component {
   }
 
   render() {
+    const bookSearch = (term) => { this.bookSearch(term); }
+
     return (
       <div>
+        <SearchBar onSearchTermChange={bookSearch} />
         <div className="row">
           <BookList
             books={this.state.books} />
