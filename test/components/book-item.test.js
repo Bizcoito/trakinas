8b0e96e9f32a05c0b35696f826c93f00ad3c0e6b
Luciano Medeiros Marcelino <marcelinolucianom@gmail.com>
2018-01-16 21:28:19 -0200

    Add specs for component properties
    And remove an useless test file

--- test/components/book-item.test.js
+++ test/components/book-item.test.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import BookItem from '../../src/components/book-item';
 import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
 
 describe('BookItem', () => {
   let book = {
     bookId: '123qwe',
     name: 'Xunda',
-    description: 'Damironga'
+    description: 'Damironga',
+    thumbnail: 'http://pix.toile-libre.org/upload/original/1499647888.png',
   }
 
   it('renders correctly', () => {
@@ -14,5 +16,13 @@ describe('BookItem', () => {
       .create(<BookItem key={book.bookId} book={book} />)
       .toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it('renders props on html', () => {
+    const component = mount(<BookItem key={book.bookId} book={book} />);
+    expect(component.find('img.media-object').props().src).toEqual(book.thumbnail);
+    expect(component.find('h4').text()).toEqual(book.name);
+    expect(component.find('p').text()).toEqual(book.description);
+    expect(component.find('BookActionButton').props().book).not.toBeNull();
   });
 });
