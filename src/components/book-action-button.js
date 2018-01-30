b3c25d8646d28b37729ab44dc8a9b00bd7aa184f
Luciano Medeiros Marcelino <marcelinolucianom@gmail.com>
2018-01-29 22:45:10 -0200

    Write everything in english :flag-en:

--- src/components/book-action-button.js
+++ src/components/book-action-button.js
@@ -21,7 +21,7 @@ class BookActionButton extends Component {
   render() {
     return (
       <button className="btn btn-info" onClick={this.onButtonClick}>
-        {this.state.book.available ? 'Pegar emprestado' : 'Return book'}
+        {this.state.book.available ? 'Borrow book' : 'Return book'}
       </button>
     );
   };
